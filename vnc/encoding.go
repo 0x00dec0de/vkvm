0c9f9155dc76ba58e65eefb46fb098a2bedae464
vase <vase>
2013-08-22 01:18:22 +0400

    try new things
    Signed-off-by: vase <vase>

--- vnc/encoding.go
+++ vnc/encoding.go
@@ -87,6 +87,7 @@ func (*RawEncoding) Read(c *Conn, rect *Rectangle, r io.Reader) (Encoding, error
 	return m, nil
 }
 
+/*
 func (enc *RawEncoding) Write(c *Conn, rect *Rectangle, w io.Writer) error {
 	var byteOrder binary.ByteOrder = binary.LittleEndian
 	if c.PixelFormat.BigEndian {
@@ -119,5 +120,76 @@ func (enc *RawEncoding) Write(c *Conn, rect *Rectangle, w io.Writer) error {
 		}
 	}
 
+	return nil
+}
+*/
+
+var (
+	pixbuf interface{}
+)
+
+func createBuf(tpe, size int) {
+	switch tpe {
+	case 8:
+		pixbuf = make([]uint8, size)
+	case 16:
+		pixbuf = make([]uint16, size)
+	case 32:
+		pixbuf = make([]uint32, size)
+	}
+}
+
+func checkBuf(tpe, size int) {
+	switch pixbuf.(type) {
+	case []uint8:
+		if tpe != 8 || len(pixbuf.([]uint8)) < size {
+			createBuf(tpe, size)
+		}
+	case []uint16:
+		if tpe != 16 || len(pixbuf.([]uint16)) < size {
+			createBuf(tpe, size)
+		}
+	case []uint32:
+		if tpe != 32 || len(pixbuf.([]uint32)) < size {
+			createBuf(tpe, size)
+		}
+	}
+}
+
+func (enc *RawEncoding) Write(c *Conn, rect *Rectangle, w io.Writer) error {
+	var byteOrder binary.ByteOrder = binary.LittleEndian
+	if c.PixelFormat.BigEndian {
+		byteOrder = binary.BigEndian
+	}
+	colors := enc.Colors
+
+	checkBuf(int(c.PixelFormat.BPP), int(rect.Width*rect.Height))
+
+	for y := uint16(0); y < rect.Height; y++ {
+		for x := uint16(0); x < rect.Width; x++ {
+			var rawPixel uint32
+			color := &colors[x*rect.Width+y]
+			if c.PixelFormat.TrueColor {
+				rawPixel = uint32(color.R<<c.PixelFormat.RedShift | color.G<<c.PixelFormat.GreenShift | color.B<<c.PixelFormat.BlueShift)
+			} else {
+
+			}
+
+			switch c.PixelFormat.BPP {
+			case 32:
+				fmt.Printf("%T %+v\n", pixbuf, pixbuf)
+				pixbuf.([]uint32)[x*rect.Width+y] = rawPixel
+			case 16:
+				pixbuf.([]uint16)[x*rect.Width+y] = uint16(rawPixel)
+			case 8:
+				pixbuf.([]uint8)[x*rect.Width+y] = uint8(rawPixel)
+			default:
+				return fmt.Errorf("TODO: BPP of %d", c.PixelFormat.BPP)
+			}
+		}
+	}
+	if err := binary.Write(w, byteOrder, pixbuf); err != nil {
+		return err
+	}
 	return nil
 }
